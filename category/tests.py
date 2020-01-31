5924cb2c858c8b2df72835ea1bd66ea11d085379
Stanislav Ch <flroup@gmail.com>
2020-01-31 19:28:01 +0600

    Recursion is added

--- category/tests.py
+++ category/tests.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
+from .models import Category
 # Create your tests here.
 
-def test_save_input_data(TestCase):
-    data_input = {"name": "Category 1",
-        "children": 
+class CategoryTest(TestCase):
+
+    def setUp(self):
+        data_input = {"name": "Category 1",
+            "children": 
             [{ "name": "Category 1.1",
                 "children": [{ "name": "Category 1.1.1",
                     "children": [{ "name": "Category 1.1.1.1"},
@@ -23,11 +27,16 @@ def test_save_input_data(TestCase):
                                             {"name": "Category 1.2.2.2"
                             }]}]}]}
 
+        response = self.client.get('/categories/', data_input)
+        print(response)
+
+
+
     data_output1 = {"id": 2, "name": "Category 1.1",
                  "parents": [{"id": 1, "name": "Category 1"}],
                  "children": [{"id": 3, "name": "Category 1.1.1"},
                  {"id": 7, "name": "Category 1.1.2"}],
-                  "siblings": [ // sisters and brothers
+                  "siblings": [                         #// sisters and brothers
                 {"id": 11, "name": "Category 1.2"}]}
 
     data_output2 = {"id": 8, "name": "Category 1.1.2.1",
--- category/views.py
+++ category/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.template import RequestContext
+import json
+
+from category.models import Category
 
 # Create your views here.
+number = 1
+
+
+@csrf_exempt
 def categoriesEndpointView(request):
-    ...
+    if request.method == 'POST':
+        json_data = json.loads(request.body)
+        print(json_data)
+        tree(json_data)
+    return HttpResponse('Got it!')
+
+def tree(json_data):
+    if not json_data:
+        return
+    global number
+    if type(json_data) == dict:
+        while json_data.get('name'):
+            print(f"{number}: {json_data.pop('name')}")
+            number += 1
+        if json_data.get('children'):
+            transfer = json_data.pop('children')
+            tree(transfer)
+    elif type(json_data) == list:
+        for item in json_data:
+            tree(item)
+
+
 
 
 def categoriesGetEndpointView(request):
-    ...
+    #if request.method == 'GET':
+    print('What you get')
