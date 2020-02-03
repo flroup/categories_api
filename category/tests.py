f79f6d36cb400c6ecdc65dcceb944d83595a9c23
Stanislav Cheshegorov <flro@yandex.ru>
2020-02-03 16:11:10 +0600

    Some corrections

--- category/tests.py
+++ category/tests.py
@@ -6,13 +6,36 @@ import json
 from .models import Category
 
 # Create your tests here.
-
 class CategoryTest(TestCase):
 
-    def setUp(self):
-        data_input = {"name":"Category 1","children":[{"name":"Category 1.1","children":[{"name":"Category 1.1.1","children":[{"name":"Category 1.1.1.1"},{"name":"Category 1.1.1.2"},{"name":"Category 1.1.1.3"}]},{"name":"Category 1.1.2","children":[{"name":"Category 1.1.2.1"},{"name":"Category 1.1.2.2"},{"name":"Category 1.1.2.3"}]}]},{"name":"Category 1.2","children":[{"name":"Category 1.2.1"},{"name":"Category 1.2.2","children":[{"name":"Category 1.2.2.1"},{"name":"Category 1.2.2.2"}]}]}]}
-        response = self.client.generic("POST", "/categories/", json.dumps(data_input))
 
+    def setUp(self):
+        data_input = {"name":"Category 1",
+                      "children":
+                          [{"name":"Category 1.1",
+                      "children":
+                          [{"name":"Category 1.1.1",
+                      "children":
+                          [{"name":"Category 1.1.1.1"},
+                           {"name":"Category 1.1.1.2"},
+                           {"name":"Category 1.1.1.3"}
+                           ]},
+                     {"name":"Category 1.1.2",
+                      "children":
+                          [{"name":"Category 1.1.2.1"},
+                           {"name":"Category 1.1.2.2"},
+                           {"name":"Category 1.1.2.3"}
+                           ]}]},
+                     {"name":"Category 1.2",
+                      "children":
+                          [{"name":"Category 1.2.1"},
+                           {"name":"Category 1.2.2",
+                      "children":
+                          [{"name":"Category 1.2.2.1"},
+                           {"name":"Category 1.2.2.2"}
+                           ]}]}]}
+        response = self.client.generic("POST", "/categories/",
+                                       json.dumps(data_input))
 
     def test_data_in_base(self):
         answer1 = Category.objects.get(id=2)
@@ -21,20 +44,37 @@ class CategoryTest(TestCase):
         self.assertEqual(answer2.number, "1.1.2.1")
 
     def test_output_view1(self):
-        data_output1 = {"id":2,"name":"Category 1.1","parents":[{"id":1,"name":"Category 1"}],"children":[{"id":3,"name":"Category 1.1.1"},{"id":7,"name":"Category 1.1.2"}],"siblings":[{"id":11,"name":"Category 1.2"}]}
+        data_output1 = {"id":2,"name":"Category 1.1",
+                        "parents":
+                                [{"id":1,"name":"Category 1"}],
+                        "children":
+                                [{"id":3,"name":"Category 1.1.1"},
+                                 {"id":7,"name":"Category 1.1.2"}],
+                        "siblings":
+                                [{"id":11,"name":"Category 1.2"}]}
         response = self.client.get("/categories/2/")
         self.assertEqual(response.status_code,200)
         self.assertEqual(json.loads(response.content), data_output1)
 
     def test_output_view2(self):
-        data_output2 = {"id":8,"name":"Category 1.1.2.1","parents":[{"id":7,"name":"Category 1.1.2"},{"id":2,"name":"Category 1.1"},{"id":1,"name":"Category 1"},],"children":[],"siblings":[{"id":9,"name":"Category 1.1.2.2"},{"id":10,"name":"Category 1.1.2.3"}]}
+        data_output2 = {"id":8,"name":"Category 1.1.2.1",
+                        "parents":
+                                [{"id":7,"name":"Category 1.1.2"},
+                                 {"id":2,"name":"Category 1.1"},
+                                 {"id":1,"name":"Category 1"},],
+                        "children": [],
+                        "siblings":
+                               [{"id":9,"name":"Category 1.1.2.2"},
+                                {"id":10,"name":"Category 1.1.2.3"}
+                                ]}
         response = self.client.get('/categories/8/')
         self.assertEqual(response.status_code,200)
         self.assertEqual(json.loads(response.content), data_output2)
 
     def test_input_wrong_data(self):
         data_input = {"name":"Category1","children":[]}
-        response = self.client.generic("POST", "/categories/", json.dumps(data_input))
+        response = self.client.generic("POST", "/categories/",
+                                       json.dumps(data_input))
         print(response.status_code, ' - check')
         # print(response.content)
         self.assertEqual(response.content, b"Wrong format input data")
--- category/views.py
+++ category/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.template import RequestContext
 import json
-import string
+
+from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
+from django.template import RequestContext
 
 from .models import Category
 
@@ -25,29 +28,36 @@ def tree(json_data):
 
     
     if type(json_data) == dict:
+
         while json_data.get("name"):
+
             try:
                 name,number = json_data.pop("name").split()
                 print(f"{number}: {name}")
             except ValueError:
                 print("Wrong format input data")
                 return "Wrong format input data"
+
             if not name.isalpha():
                 print(f"It should have only letters - {name}")
                 return
+
             if not all([n in '.1234567890' for n in number]):
                 print(f"Wrong number of tree - {number}")
+
             try:
                 result = Category.objects.get(number=number)
             except Category.DoesNotExist:
                 result = None
+
             if not result or not result.name == name:
-    
                 obj = Category(name=name, number=number)
                 obj.save()
+
         if json_data.get("children"):
             transfer = json_data.pop("children")
             tree(transfer)
+
     elif type(json_data) == list:
         for item in json_data:
             tree(item)
@@ -57,13 +67,15 @@ def tree(json_data):
 #Answer for a request by GET on /categories/<category_id>/
 def categoriesGetEndpointView(request, category_id):
     answer = result = ''
+
     if request.method == 'GET':
+
         try:
             answer = Category.objects.get(id=category_id)
         except Category.DoesNotExist:
             answer = None
+
         if answer:
-    
             result = backfire(category_id, answer)
     
     return HttpResponse(json.dumps(result), content_type='application/json')
@@ -75,21 +87,30 @@ def backfire(category_id, etalon):
 
     parents, children, siblings = [], [], []
     txt = '.'.join(etalon.number.split('.')[:-1])
-    # print(f"etalon == {etalon.number}, txt == {txt}")
     array = Category.objects.filter(name=etalon.name, number__startswith=txt)
-    parray = ['.'.join(etalon.number.split('.')[0:i+1]) for i in range(etalon.number.count('.'))][::-1]
-    # print(parray)
-    parents = [{'id':Category.objects.get(number=relative).id, 'name':etalon.name + ' ' + Category.objects.get(number=relative).number} for relative in parray if Category.objects.filter(name=etalon.name, number=relative)]
-    # print(f"parents == {parents}")
+
+    parray = ['.'.join(etalon.number.split('.')[0:i+1])
+              for i in range(etalon.number.count('.'))][::-1]
+
+    parents = [{'id':Category.objects.get(number=relative).id,
+                'name':etalon.name + ' ' + Category.objects.get(number=relative).number}
+               for relative in parray
+               if Category.objects.filter(name=etalon.name, number=relative)]
+
     for relative in array:
         rdot = relative.number.count('.')
         edot = etalon.number.count('.')
         
         if rdot == edot and relative.number != etalon.number:
-            siblings.append({'id':relative.id, 'name': etalon.name + " " + relative.number})
+            siblings.append({'id':relative.id,
+                             'name': etalon.name + " " + relative.number})
 
-        elif rdot == edot + 1 and etalon.number == relative.number[:relative.number.rfind('.')]:
-            children.append({'id':relative.id, 'name': etalon.name + " " + relative.number})
-    return {'id': category_id, 'name': etalon.name + ' ' + etalon.number,'parents': parents, 'children':children, 'siblings':siblings}
+        elif rdot == edot + 1 and \
+                        etalon.number == relative.number[:relative.number.rfind('.')]:
+            children.append({'id':relative.id,
+                             'name': etalon.name + " " + relative.number})
+
+    return {'id': category_id, 'name': etalon.name + ' ' + etalon.number,
+            'parents': parents, 'children':children, 'siblings':siblings}
 
 
